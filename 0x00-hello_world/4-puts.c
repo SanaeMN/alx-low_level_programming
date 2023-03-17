5945286402e3ec6ba5d6e7e559ba0fe3b9855797
MOUMENE Sanae <sanaeenemuom2017@gmail.com>
2023-03-16 17:44:02 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-"include<stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
  *
  * Return: always 0(Success)
  */
-
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
