d32ed8b08a73bdac826eba3a5140c64d5c3a6ac3
MOUMENE Sanae <sanaeenemuom2017@gmail.com>
2023-03-23 19:35:25 -0700

    0.Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void postitive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is poditive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+		printf("%d is zero\n", i);
+}
