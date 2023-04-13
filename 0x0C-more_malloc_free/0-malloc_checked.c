63784d8619b71ffd2f01038b3d5c84bbbb7d7148
MOUMENE Sanae <sanaeenemuom2017@gmail.com>
2023-04-13 13:07:30 -0700

    gp

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* *malloc_checked - allocates memory
+* @b: unsigned int
+* Return: pointer to the allocated memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *n;
+
+	n = malloc(b);
+
+	if (n == NULL)
+	exit(98);
+
+	return (n);
+}
