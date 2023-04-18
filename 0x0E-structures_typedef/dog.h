56a15555427a7d2aba8a3045aca1d120fc9eb905
MOUMENE Sanae <sanaeenemuom2017@gmail.com>
2023-04-17 17:05:32 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* struct dog -  dog struct
+* @name: the name of the dog
+* @age: the age of the dog
+* @owner: the name of the owner
+* Description: the attributes of a dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
