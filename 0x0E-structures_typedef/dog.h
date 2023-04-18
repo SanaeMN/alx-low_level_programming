07579f5b3d8b5ed39a91a56edaf86c49ee5d4a0a
MOUMENE Sanae <sanaeenemuom2017@gmail.com>
2023-04-17 18:06:18 -0700

    2. A dog will teach you unconditional love.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
 * struct dog -  dog struct
@@ -17,5 +17,8 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/*dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);*/
 
-#endif
+#endif /* DOG_H */
