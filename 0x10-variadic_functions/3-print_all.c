c3351c7a9080e0aa6f8e6d014ac42b755b26afa6
MOUMENE Sanae <sanaeenemuom2017@gmail.com>
2023-04-20 20:16:58 -0700

    3. To be is a to be the value of a variable.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,54 +3,89 @@
 #include <stdio.h>
 
 /**
+* format_char - format character.
+* @separator: separator strings.
+* @ap: argument pointer.
+*/
+void format_char(char *separator, va_list ap)
+{
+	printf("%s%c", separator, va_arg(ap, int));
+}
+
+/**
+* format_int - format integer.
+* @separator: separator strings.
+* @ap: argument pointer.
+*/
+void format_int(char *separator, va_list ap)
+{
+	printf("%s%d", separator, va_arg(ap, int));
+}
+
+/**
+* format_float - format float.
+* @separator: separator strings.
+* @ap: argument pointer.
+*/
+void format_float(char *separator, va_list ap)
+{
+	printf("%s%f", separator, va_arg(ap, double));
+}
+
+/**
+* format_string - format string.
+* @separator: separator strings.
+* @ap: argument pointer.
+*/
+void format_string(char *separator, va_list ap)
+{
+	char *s = va_arg(ap, char*);
+
+	switch ((int)(!s))
+	case 1:
+		s = "(nil)";
+		printf("%s%s", separator, s);
+}
+
+/**
 * print_all - function that prints anything.
-* @format: number of strings.
+* @format: format of strings.
 * Return: anything.
 */
+typedef struct token {
+	char *token;
+	void (*f)(char *, va_list);
+}token_t;
 
 void print_all(const char * const format, ...)
 {
+	int i = 0, j;
+	char *separator = "";
 	va_list ap;
-	int i = 0;
-	char *s;
+
+	token_t tokens[] = {
+		{"c", format_char},
+		{"i", format_int},
+		{"f", format_float},
+		{"s", format_string},
+		{NULL, NULL}
+	};
 
 	va_start(ap, format);
-
-	while (format != NULL && format[i] != '\0')
+	while (format && format[i])
 	{
-		switch (format[i])
+		j = 0;
+		while (tokens[j].token)
 		{
-			case 'c':
-				printf("%c", va_arg(ap, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(ap, int));
-				break;
-			case 'f':
-				printf("%f", (float) va_arg(ap, double));
-				break;
-			case 's':
-				s = va_arg(ap, char *);
-				if (s == NULL)
-				{
-					printf("(nil)");
-				}
-				else
-				{
-					printf("%s", s);
-				}
-				break;
-			default:
-				break;
+			if (format[i] == tokens[j].token[0])
+			{
+				tokens[j].f(separator, ap);
+				separator = ", ";
+			}
+			j++;
 		}
 		i++;
-		if (format[i] != '\0' && (format[i] == 'c' || format[i] == 'i' ||
-					format[i] == 'f' || format[i] == 's'))
-		{
-			printf(", ");
-		}
 	}
-
-	va_end(ap);
 	printf("\n");
+	va_end(ap);
 }
